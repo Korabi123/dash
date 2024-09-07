d658a53a8d7a5ec734592780f22cf88cb1b92c71
Korab Imeri <korabimeri91@gmail.com>
2024-09-07 15:49:44 +0200

    refactor: removed unused imports

--- app/(routes)/dashboard/(routes)/transactions/page.tsx
+++ app/(routes)/dashboard/(routes)/transactions/page.tsx
@@ -1,9 +1,4 @@
 import { FilterButtons } from "@/components/dashboard/filter-buttons";
-import { DataTable } from "@/components/dashboard/tables/data-table";
-import { transactionColumns } from "@/types/transaction-columns";
-
-import { ArrowUpRightFromSquare, PlusIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { TransactionsTable } from "@/components/dashboard/tables/transactions-table";
 
 const DashboardOverviewPage = () => {
